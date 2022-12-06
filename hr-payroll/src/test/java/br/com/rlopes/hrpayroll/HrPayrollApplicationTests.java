4f4907b502362b4c6f90808dea1536c1776d8266
Ricardo Lopes <prslopes1@gmail.com>
2022-12-06 12:24:04 -0300

    build: project hr-payroll created.

---
+++ hr-payroll/src/test/java/br/com/rlopes/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.rlopes.hrpayroll;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HrPayrollApplicationTests {
+
+	@Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
