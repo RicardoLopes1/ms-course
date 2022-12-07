d2e85b34a828971de05e51c45a2cea58f9cd018a
Ricardo Lopes <prslopes1@gmail.com>
2022-12-07 16:48:42 -0300

    build: Project hr-eureka-server created.

---
+++ hr-eureka-server/src/test/java/br/com/rlopes/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.rlopes.hreurekaserver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HrEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
