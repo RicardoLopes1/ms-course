505213ed1ede9ff99ffb3c633142f4815bd3065a
Ricardo Lopes <prslopes1@gmail.com>
2022-12-08 10:40:59 -0300

    build: Project hr-api-gateway-zuul created.

---
+++ hr-api-gateway-zuul/src/test/java/br/com/rlopes/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.rlopes.hrapigatewayzuul;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
