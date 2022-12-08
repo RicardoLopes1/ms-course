32fcbff02d23452e1d0f167818a0fb3827224feb
Ricardo Lopes <prslopes1@gmail.com>
2022-12-08 14:56:46 -0300

    build: Project hr-config-server created.

---
+++ hr-config-server/src/test/java/br/com/rlopes/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.rlopes.hrconfigserver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HrConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
