923562c41228c0839b61241ca7681b2be2253d54
Ricardo Lopes <prslopes1@gmail.com>
2022-12-09 16:03:37 -0300

    build: Project hr-user created.

---
+++ hr-user/src/test/java/br/com/rlopes/hruser/HrUserApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.rlopes.hruser;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HrUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
