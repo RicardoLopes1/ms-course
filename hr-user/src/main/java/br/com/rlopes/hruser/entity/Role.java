b531bb14d916e9a96b7044b0b39d0c6722fd2b0d
Ricardo Lopes <prslopes1@gmail.com>
2022-12-09 17:00:04 -0300

    feat: User, Role, many to many association.

---
+++ hr-user/src/main/java/br/com/rlopes/hruser/entity/Role.java
@@ -0,0 +1,31 @@
+package br.com.rlopes.hruser.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+}
