a102d7ed788e75a74f56c31c438898ab1b4a8b51
Ricardo Lopes <prslopes1@gmail.com>
2022-12-12 10:53:09 -0300

    build: Project hr-oauth created.

---
+++ hr-oauth/src/test/java/br/com/rlopes/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.rlopes.hroauth;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HrOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
