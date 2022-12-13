5670621ff4af64d0dc69f8af1b69cf2213865f13
Ricardo Lopes <prslopes1@gmail.com>
2022-12-13 10:54:04 -0300

    feat: Login, JWT token.

--- hr-oauth/src/main/java/br/com/rlopes/hroauth/entity/User.java
+++ hr-oauth/src/main/java/br/com/rlopes/hroauth/entity/User.java
@@ -1,8 +1,13 @@
 package br.com.rlopes.hroauth.entity;
 
-import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User implements Serializable {
+public class User implements UserDetails {
 
     private static final long serialVersionUID = 1L;
 
@@ -20,4 +25,36 @@ public class User implements Serializable {
     private String email;
     private String password;
     private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- hr-oauth/src/main/java/br/com/rlopes/hroauth/service/UserService.java
+++ hr-oauth/src/main/java/br/com/rlopes/hroauth/service/UserService.java
@@ -1,5 +1,8 @@
 package br.com.rlopes.hroauth.service;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.rlopes.hroauth.entity.User;
@@ -10,10 +13,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @AllArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private UserFeignClient userFeignClient;
 
+    // this is a didactic example, in a real world scenario, you should use loadUserByUsername method
     public User findByEmail(String email) {
         var user = userFeignClient.findByEmail(email).getBody();
         if (user == null) {
@@ -22,6 +26,18 @@ public class UserService {
         }
 
         log.info("Email found: " + email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userFeignClient.findByEmail(username).getBody();
+        if (user == null) {
+            log.error("Email not found: " + username);
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        log.info("Email found: " + username);
         return user;
     }
 }
